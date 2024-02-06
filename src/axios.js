e5b002b01370d657890be8519655802a7819924a
Bhavik Gevariya <bgevariya20@gmail.com>
2024-02-06 10:59:07 -0500

    bug solved
    - import csv files
    - menubar title changed
    - made address field not required

--- src/axios.js
+++ src/axios.js
@@ -1,15 +1,17 @@
 import axios from "axios";
 // console.log("API URL:", process.env.REACT_APP_API_URL);
-axios.defaults.baseURL = "http://localhost:8000/api";
-// axios.defaults.baseURL = "https://brainy-gown-foal.cyclic.cloud/api";
+//axios.defaults.baseURL = "http://localhost:8000/api";
+axios.defaults.baseURL = "https://brainy-gown-foal.cyclic.cloud/api";
 
-export const getAuthTokenFromLocalStorage = () => localStorage.getItem("auth-token");
+export const getAuthTokenFromLocalStorage = () =>
+  localStorage.getItem("auth-token");
 
 export const setTokenInHeader = () => {
-    axios.defaults.headers.common["x-auth-token"] = getAuthTokenFromLocalStorage();
+  axios.defaults.headers.common["x-auth-token"] =
+    getAuthTokenFromLocalStorage();
 };
 
 export const setAuthTokenToLocalStorage = (authToken) => {
-    localStorage.setItem("auth-token", authToken);
-    setTokenInHeader();
+  localStorage.setItem("auth-token", authToken);
+  setTokenInHeader();
 };
--- src/components/placement-locations/index.js
+++ src/components/placement-locations/index.js
@@ -1,6 +1,4 @@
-import React, {
-    useCallback, useEffect, useMemo, useRef
-} from "react";
+import React, { useCallback, useEffect, useMemo, useRef } from "react";
 import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
 import DeleteIcon from "@mui/icons-material/Delete";
@@ -14,311 +12,274 @@ import UploadIcon from "@mui/icons-material/Upload";
 import { useStore } from "../../store";
 import Table from "../common/Table";
 import ShowConfirmDialog from "../common/ConfirmDialog";
-import ShowSnackbarAlert, { ShowErrorAlert, ShowSuccessAlert } from "../common/SnackBarAlert";
+import ShowSnackbarAlert, {
+  ShowErrorAlert,
+  ShowSuccessAlert,
+} from "../common/SnackBarAlert";
 import ShowDialog from "../common/Dialog";
 import UploadFile from "../common/UploadFile";
 import { PLACEMENT_LOCATIONS_REQUIRED_HEADERS } from "../utils";
 
 function PlacementLocations() {
-    const { placementLocationStore } = useStore();
-    const tableInstanceRef = useRef(null);
-    const uploadFileRef = useRef(null);
-    const navigate = useNavigate();
+  const { placementLocationStore } = useStore();
+  const tableInstanceRef = useRef(null);
+  const uploadFileRef = useRef(null);
+  const navigate = useNavigate();
 
-    const { list, totalCount, fetched } = placementLocationStore;
+  const { list, totalCount, fetched } = placementLocationStore;
 
-    const handleAddClick = useCallback(() => {
-        navigate({ pathname: "add" });
-    }, []);
+  const handleAddClick = useCallback(() => {
+    navigate({ pathname: "add" });
+  }, []);
 
-    const handleImportClick = useCallback(() => {
-        ShowDialog({
-            title: "Import Hospitals",
-            actionBtnName: "Import",
-            content: <UploadFile
-                ref={uploadFileRef}
-                headers={PLACEMENT_LOCATIONS_REQUIRED_HEADERS}
-            />,
-            onConfirm: async () => {
-                const fileData = await uploadFileRef.current.getFileData();
-                if (fileData) {
-                    try {
-                        await placementLocationStore.import(fileData);
-                        ShowSuccessAlert("Imported successfully");
-                    } catch (err) {
-                        ShowErrorAlert(err.message);
-                    }
-                } else {
-                    ShowErrorAlert("Please select the file", 3000);
-                }
-            }
-        });
-    }, []);
+  const handleImportClick = useCallback(() => {
+    ShowDialog({
+      title: "Import Hospitals",
+      actionBtnName: "Import",
+      content: (
+        <UploadFile
+          ref={uploadFileRef}
+          headers={PLACEMENT_LOCATIONS_REQUIRED_HEADERS}
+        />
+      ),
+      onConfirm: async () => {
+        const fileData = await uploadFileRef.current.getFileData();
+        if (fileData) {
+          try {
+            await placementLocationStore.import(fileData);
+            ShowSuccessAlert("Imported successfully");
+          } catch (err) {
+            ShowErrorAlert(err.message);
+          }
+        } else {
+          ShowErrorAlert("Please select the file", 3000);
+        }
+      },
+    });
+  }, []);
 
-    const handleEditRow = useCallback((index) => {
-        const locationsList = toJS(placementLocationStore.list);
-        navigate({
-            pathname: "edit",
-            search: createSearchParams({
-                id: locationsList[index]._id
-            }).toString()
-        });
-    }, []);
+  const handleEditRow = useCallback((index) => {
+    const locationsList = toJS(placementLocationStore.list);
+    navigate({
+      pathname: "edit",
+      search: createSearchParams({
+        id: locationsList[index]._id,
+      }).toString(),
+    });
+  }, []);
 
-    const handleDeleteRow = useCallback((index) => {
-        ShowConfirmDialog({
-            title: "Delete Placement Location",
-            description: "Are you sure you want to delete this placement location?",
-            actionBtnName: "Delete",
-            onConfirm: async () => {
-                try {
-                    await placementLocationStore.delete(index);
-                    ShowSnackbarAlert({
-                        message: "Deleted successfully"
-                    });
-                } catch (err) {
-                    ShowSnackbarAlert({
-                        message: err.message, severity: "error"
+  const handleDeleteRow = useCallback((index) => {
+    ShowConfirmDialog({
+      title: "Delete Placement Location",
+      description: "Are you sure you want to delete this placement location?",
+      actionBtnName: "Delete",
+      onConfirm: async () => {
+        try {
+          await placementLocationStore.delete(index);
+          ShowSnackbarAlert({
+            message: "Deleted successfully",
+          });
+        } catch (err) {
+          ShowSnackbarAlert({
+            message: err.message,
+            severity: "error",
+          });
+        }
+      },
+    });
+  }, []);
 
-                    });
-                }
-            }
-        });
-    }, []);
+  const handleDeleteSelectedRows = useCallback((selectedRows) => {
+    ShowConfirmDialog({
+      title: "Delete Placement Locations",
+      description: `Are you sure you want to delete these ${selectedRows.length} placement locations(s)?`,
+      actionBtnName: "Delete",
+      onConfirm: async () => {
+        try {
+          await placementLocationStore.deleteMultiple(selectedRows);
+          ShowSnackbarAlert({
+            message: "Deleted successfully",
+          });
+        } catch (err) {
+          ShowSnackbarAlert({
+            message: err.message,
+            severity: "error",
+          });
+        }
+      },
+    });
+  }, []);
 
-    const handleDeleteSelectedRows = useCallback((selectedRows) => {
-        ShowConfirmDialog({
-            title: "Delete Placement Locations",
-            description: `Are you sure you want to delete these ${selectedRows.length} placement locations(s)?`,
-            actionBtnName: "Delete",
-            onConfirm: async () => {
-                try {
-                    await placementLocationStore.deleteMultiple(selectedRows);
-                    ShowSnackbarAlert({
-                        message: "Deleted successfully"
-                    });
-                } catch (err) {
-                    ShowSnackbarAlert({
-                        message: err.message, severity: "error"
+  const getToolBarActions = useCallback(
+    () => (
+      <Box>
+        <Button startIcon={<AddIcon />} onClick={handleAddClick}>
+          Add
+        </Button>
+        <Button startIcon={<UploadIcon />} onClick={handleImportClick}>
+          Import
+        </Button>
+      </Box>
+    ),
+    []
+  );
 
-                    });
-                }
-            }
-        });
-    }, []);
+  const columns = useMemo(
+    () => [
+      {
+        name: "name",
+        label: "Name",
+      },
+      {
+        name: "hospital",
+        label: "Campus",
+        // options: {
+        //     // eslint-disable-next-line react/no-unstable-nested-components
+        //     customBodyRender: (value) => {
+        //         if (value) {
+        //             return value.campus;
+        //             // (
+        //             //     <div>
+        //             //         {`${value.name}, ${value.campus}`}
+        //             //         <br />
+        //             //         {`${value.address}`}
+        //             //     </div>
+        //             // );
+        //         }
 
-    const getToolBarActions = useCallback(() => (
-        <Box>
-            <Button
-                startIcon={<AddIcon />}
-                onClick={handleAddClick}
-            >
-                Add
-            </Button>
-            <Button
-                startIcon={<UploadIcon />}
-                onClick={handleImportClick}
-            >
-                Import
-            </Button>
-        </Box>
-    ), []);
+        //         return "";
+        //     }
+        // }
+      },
+      {
+        name: "instructor",
+        label: "Instructor",
+        // options: {
+        //     // eslint-disable-next-line react/no-unstable-nested-components
+        //     customBodyRender: (value) => {
+        //         if (value) {
+        //             return `${value.fname} ${value.lname}`;
+        //             // return (
+        //             //     <div>
+        //             //         {`${value.fname} ${value.lname}, ${value.number}`}
+        //             //         <br />
+        //             //         {`${value.email}`}
+        //             //     </div>
+        //             // );
+        //         }
 
-    const CustomTableCell = ({ value }) => {
-        return (
-            <Tooltip 
-                title={value} 
-                arrow 
-                placement="top"
-            >
-                <div 
-                    style={{
-                        overflow: "hidden",
-                        textOverflow: "ellipsis",
-                        whiteSpace: "nowrap"
-                        // maxWidth: "100px" 
-                    }}
-                >
-                    {value}
-                </div>
-            </Tooltip>
-        );
+        //         return "";
+        //     }
+        // }
+      },
+      {
+        name: "unit",
+        label: "Unit",
+      },
+      {
+        name: "section",
+        label: "Section",
+      },
+      {
+        name: "day",
+        label: "Day",
+      },
+      {
+        name: "shift",
+        label: "Shift",
+      },
+      {
+        name: "seats",
+        label: "Seats",
+      },
+      {
+        name: "",
+        options: {
+          viewColumns: false,
+          setCellProps: () => ({ style: { width: "100px" } }),
+          // eslint-disable-next-line react/no-unstable-nested-components
+          customBodyRender: (value, tableMeta) => (
+            <Box sx={{ display: "flex", gap: "1rem" }}>
+              <Tooltip title="Edit" arrow placement="left">
+                <EditIcon
+                  color="action"
+                  className="actionIcon"
+                  fontSize="small"
+                  sx={{ cursor: "pointer" }}
+                  onClick={() => handleEditRow(tableMeta.rowIndex)}
+                />
+              </Tooltip>
+              <Tooltip title="Delete" arrow placement="right">
+                <DeleteIcon
+                  color="error"
+                  className="actionIcon"
+                  fontSize="small"
+                  sx={{ cursor: "pointer" }}
+                  onClick={() => handleDeleteRow(tableMeta.rowIndex)}
+                />
+              </Tooltip>
+            </Box>
+          ),
+        },
+      },
+    ],
+    []
+  );
+
+  useEffect(() => {
+    const fetchAll = async () => {
+      if (!fetched) {
+        await placementLocationStore.fetchAll();
+      }
     };
 
-    const columns = useMemo(() => [
-        {
-            name: "name", label: "Name", options: { filter: false, setCellProps: () => ({ style: { minWidth: "150px", maxWidth: "150px", width: "150px" }}), customBodyRender: (value) => (
-                <div style={{
-                    whiteSpace: "nowrap",
-                    overflow: "hidden",
-                    textOverflow: "ellipsis",
-                    boxSizing: "border-box"
-                }}>
-                    <CustomTableCell value={value} />
-                </div>
-            ) }
-        },
-        {
-            name: "hospital",
-            label: "Campus", options: { filter: false, setCellProps: () => ({ style: { minWidth: "200px", maxWidth: "200px", width: "200px" }}), customBodyRender: (value) => (
-                <div style={{
-                    whiteSpace: "nowrap",
-                    overflow: "hidden",
-                    textOverflow: "ellipsis",
-                    boxSizing: "border-box"
-                }}>
-                    <CustomTableCell value={value} />
-                </div>
-            ) }
-            // options: {
-            //     // eslint-disable-next-line react/no-unstable-nested-components
-            //     customBodyRender: (value) => {
-            //         if (value) {
-            //             return value.campus;
-            //             // (
-            //             //     <div>
-            //             //         {`${value.name}, ${value.campus}`}
-            //             //         <br />
-            //             //         {`${value.address}`}
-            //             //     </div>
-            //             // );
-            //         }
+    fetchAll();
+  }, []);
 
-            //         return "";
-            //     }
-            // }
-        },
-        {
-            name: "instructor",
-            label: "Instructor", options: { filter: false, setCellProps: () => ({ style: { minWidth: "150px", maxWidth: "150px", width: "150px" }}), customBodyRender: (value) => (
-                <div style={{
-                    whiteSpace: "nowrap",
-                    overflow: "hidden",
-                    textOverflow: "ellipsis",
-                    boxSizing: "border-box"
-                }}>
-                    <CustomTableCell value={value} />
-                </div>
-            ) }
-            // options: {
-            //     // eslint-disable-next-line react/no-unstable-nested-components
-            //     customBodyRender: (value) => {
-            //         if (value) {
-            //             return `${value.fname} ${value.lname}`;
-            //             // return (
-            //             //     <div>
-            //             //         {`${value.fname} ${value.lname}, ${value.number}`}
-            //             //         <br />
-            //             //         {`${value.email}`}
-            //             //     </div>
-            //             // );
-            //         }
+  const tableData = toJS(list).map((obj) => ({
+    ...obj,
+    hospital: (obj.hospital && obj.hospital.campus) || "test",
+    instructor: `${
+      obj.instructor && obj.instructor.fname
+        ? obj.instructor && obj.instructor.fname
+        : "fname"
+    } ${
+      obj.instructor && obj.instructor.lname
+        ? obj.instructor && obj.instructor.lname
+        : "lname"
+    }`,
+  }));
 
-            //         return "";
-            //     }
-            // }
-        },
-        {
-            name: "unit", label: "Unit", options: { filter: false, setCellProps: () => ({ style: { textAlign: "center" }})}
-        },
-        {
-            name: "section", label: "Section", options: { filter: false, setCellProps: () => ({ style: { textAlign: "center" }})}
-        },
-        {
-            name: "day", label: "Day"
-        },
-        {
-            name: "shift", label: "Shift"
-        },
-        {
-            name: "seats", label: "Seats", options: { filter: false, setCellProps: () => ({ style: { textAlign: "center" }})}
-        },
-        {
-            name: "",
-            options: {
-                viewColumns: false,
-                setCellProps: () => ({ style: { width: "100px" } }),
-                // eslint-disable-next-line react/no-unstable-nested-components
-                customBodyRender: (value, tableMeta) => (
-                    <Box sx={{ display: "flex", gap: "1rem" }}>
-                        <Tooltip
-                            title="Edit"
-                            arrow
-                            placement="left"
-                        >
-                            <EditIcon
-                                color="action"
-                                className="actionIcon"
-                                fontSize="small"
-                                sx={{ cursor: "pointer" }}
-                                onClick={() => handleEditRow(tableMeta.rowIndex)}
-                            />
-                        </Tooltip>
-                        <Tooltip
-                            title="Delete"
-                            arrow
-                            placement="right"
-                        >
-                            <DeleteIcon
-                                color="error"
-                                className="actionIcon"
-                                fontSize="small"
-                                sx={{ cursor: "pointer" }}
-                                onClick={() => handleDeleteRow(tableMeta.rowIndex)}
-                            />
-                        </Tooltip>
-                    </Box>
-                )
-            }
-        }
-    ], []);
-
-    useEffect(() => {
-        const fetchAll = async () => {
-            if (!fetched) {
-                await placementLocationStore.fetchAll();
-            }
-        };
-
-        fetchAll();
-    }, []);
-
-    const tableData = toJS(list).map((obj) => ({
-        ...obj,
-        hospital: obj.hospital.campus,
-        instructor: `${obj.instructor.fname} ${obj.instructor.lname}`
-    }));
-
-    return (
-        <Box>
-            <Table
-                key={`${Date.now()}`}
-                ref={tableInstanceRef}
-                data={tableData}
-                columns={columns}
-                title={getToolBarActions()}
-                options={{
-                    viewColumns: false,
-                    search: false,
-                    filter: false,
-                    count: totalCount,
-                    onRowsDelete: ({ data }) => {
-                        const indexes = data.map((obj) => obj.index);
-                        handleDeleteSelectedRows(indexes);
-                        return false;
-                    },
-                    downloadOptions: {
-                        filename: "Placement Locations"
-                    },
-                    textLabels: {
-                        body: {
-                            noMatch: !fetched ? "Fetching..." : "No records found"
-                        }
-                    }
-                }}
-            />
-        </Box>
-    );
+  return (
+    <Box>
+      <Table
+        key={`${Date.now()}`}
+        ref={tableInstanceRef}
+        data={tableData}
+        columns={columns}
+        title={getToolBarActions()}
+        options={{
+          viewColumns: false,
+          search: false,
+          filter: false,
+          count: totalCount,
+          onRowsDelete: ({ data }) => {
+            const indexes = data.map((obj) => obj.index);
+            handleDeleteSelectedRows(indexes);
+            return false;
+          },
+          downloadOptions: {
+            filename: "Placement Locations",
+          },
+          textLabels: {
+            body: {
+              noMatch: !fetched ? "Fetching..." : "No records found",
+            },
+          },
+        }}
+      />
+    </Box>
+  );
 }
 
 export default observer(PlacementLocations);
